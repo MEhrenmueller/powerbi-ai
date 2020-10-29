4461cd86d17107f194e98d067b0e00b0c189c9a7
MEhrenmueller <markus.ehrenmueller@gmail.com>
2020-10-29 15:54:34 +0100

    Add files via upload

---
+++ libraries.R
@@ -0,0 +1,18 @@
+lib <- "C:\\Program Files\\R\\R-3.5.1\\library"
+
+install.packages("corrplot", lib)
+
+
+install.packages("corrplot")
+
+install.packages("devtools")
+#install.packages("devtools", lib = lib, repos="https://mran.microsoft.com/")
+library(devtools)
+install.packages("BoomSpikeSlab") #, repos="https://mran.microsoft.com/")
+install.packages("dtw") #, repos="https://mran.microsoft.com/")
+install.packages("chron") #, repos="https://mran.microsoft.com/")
+devtools::install_github("google/CausalImpact")
+
+install.packages("CausalImpact")
+install.packages("data.table")
+
